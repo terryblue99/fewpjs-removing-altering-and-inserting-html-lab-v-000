eeb7dc4877b1f27a3dd5c367d1acaf7f12d99b44
Terry Blue <terryblue@comcast.net>
2020-02-15 21:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
 // Write your code here!
-document.body.appendChild(element)
-
-let ul = document.createElement('ul')
-
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-element.appendChild(ul)
-
-let element = document.querySelector("p#greeting");
-element.innerHTML = 'Hello, DOM!'
+main.removeChild(main#main);
