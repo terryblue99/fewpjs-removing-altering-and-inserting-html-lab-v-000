a7207adf99b086c0a8389e1ff91a83d11db2dc91
Terry Blue <terryblue@comcast.net>
2020-02-15 22:48:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let el = document.getElementById('main');
+  el.remove();
+
+
+let newHeader = document.createElement('h1');
+    newHeader.id = 'victory'
+    newHeader.innerHTML = 'Terry is the champion';
+    
